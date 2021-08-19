237a8b7d83ee350f9b13f16b27e8548229ded5f2
barbara.sousa <barbara.sousa@appprova.com.br>
2021-08-19 03:00:03 -0300

    add lambda, glue_crawler

--- upload_file_to_s3.py
+++ upload_file_to_s3.py
@@ -4,8 +4,10 @@ s3_client = boto3.client('s3')
 butcket_name = 'datalake-barbara'
 s3_folder_name = 'raw-data/censo'
 local_folder_name = 'Data/DADOS'
-files_to_upload = ['matricula_co.csv', 'matricula_nordeste.csv', 'matricula_norte.csv',
-                   'matricula_sudeste.csv', 'matricula_sul.csv'] 
+files_to_upload = [
+    # 'matricula_co.csv', 'matricula_nordeste.csv', 'matricula_norte.csv',
+    'matricula_sudeste.csv', 'matricula_sul.csv'
+]
 
 for file_name in files_to_upload:
     s3_client.upload_file(f'{local_folder_name}/{file_name}',
