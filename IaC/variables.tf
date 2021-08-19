9d989524ab1774b233fdac084422ed361c21ec4b
barbara.sousa <barbara.sousa@appprova.com.br>
2021-08-19 02:57:39 -0300

    add lambda, glue_crawler

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -1,3 +1,15 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
 variable "base_bucket_name" {
   default = "datalake-igti-tf"
+}
+
+variable "lambda_function_name" {
+  default = "IGTIexecutaEMR"
+}
+
+variable "key_pair_name" {
+  default = "key-igti-teste"
 }
